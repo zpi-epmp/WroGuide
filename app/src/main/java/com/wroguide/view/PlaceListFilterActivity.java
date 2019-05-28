463a867bc08396fe762e80e0e1581417cf32d43b
adety89 <169447@student.pwr.edu.pl>
2019-05-28 19:53:22 +0200

    Merge branch 'master' of https://github.com/zpi-epmp/WroGuide
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/wroguide/view/PlaceListFilterActivity.java
+++ app/src/main/java/com/wroguide/view/PlaceListFilterActivity.java
@@ -1,80 +1,52 @@
 package com.wroguide.view;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 import com.wroguide.R;
+import com.wroguide.presenter.CheckingClass;
 
 
+public class PlaceListFilterActivity extends AppCompatActivity {
 
-public class PlaceListFilterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Spinner spinner;
-    TextInputLayout inputTekst;
+    AutoCompleteTextView inputTekst;
     int choice;
     String objectName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
-        spinner = (Spinner) findViewById(R.id.spinner);
+
         inputTekst= findViewById(R.id.textInput);
-        inputTekst.setVisibility(View.INVISIBLE);
-        Resources res = getResources();
-        String[] filterOptions = res.getStringArray(R.array.filterOptions);
-
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.filterOptions,
-                R.layout.my_spinner);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-        }
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch(position)
-        {
-            case 0:
-                choice=0;
-
-                break;
-            case 1:
-                choice=1;
-
-                break;
-            case 2:
-                inputTekst.setVisibility(View.VISIBLE);
-                choice=2;
-
-                break;
+       // inputTekst.setHelperTextColor();
+        inputTekst.setVisibility(View.VISIBLE);
 
         }
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        choice=0;
 
-    }
+
 
 
     public void goToFiltered(View w) {
 
-        final Intent int4 = new Intent(this, PlaceListFilteredActivity.class);
-        objectName=inputTekst.getEditText().getText().toString();
-        int4.putExtra("choice",choice);
+
+        objectName=inputTekst.getText().toString();
+        CheckingClass checker=new CheckingClass();
+        boolean checkInfo=checker.CheckPresenceOfObject(objectName);
+        if(checkInfo){
+            final Intent int4 = new Intent(this, PlaceListFilteredActivity.class);
+        int4.putExtra("choice",3);
         int4.putExtra("objectName",objectName);
-        startActivity(int4);
+        startActivity(int4);}
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.ToastText), Toast.LENGTH_LONG);
+            toast.show();
+            inputTekst.setText("");
+        }
     }
 }
 
