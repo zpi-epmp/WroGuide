dbc12f5b2f979f44f5e06059c15e25b425e1bb24
adety89 <169447@student.pwr.edu.pl>
2019-06-01 17:35:03 +0200

    dodanie aktywnosci tworzenia nowej trasy

--- app/src/main/java/com/wroguide/model/Routes.java
+++ app/src/main/java/com/wroguide/model/Routes.java
@@ -1,17 +1,19 @@
 package com.wroguide.model;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by Piotrek on 17.03.2019.
  */
-public class Routes {
+public class Routes implements Serializable {
     private List<Route> routes;
 
     public Routes(RouteDAO dao) {
         routes = dao.getAll();
     }
-
+    public Routes() {
+    }
     public List<Route> getRoutes() {
         return routes;
     }
